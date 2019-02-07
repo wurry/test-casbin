93ba5068bb52514d61d6ad8460db82c68e907b3a
wurry <31262978+wurry@users.noreply.github.com>
2019-02-07 15:01:27 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -3,9 +3,33 @@ package main
 import (
 	"fmt"
 	"github.com/casbin/casbin"
+	"github.com/kataras/iris"
 )
 
-func main(){
+func main() {
+	app := iris.Default()
+	app.Get("/ping", func(ctx iris.Context) {
+		x:= ctx.URLParam("firstname")
+		y:=ctx.URLParam("lastname")
+		ctx.JSON(iris.Map{
+			"message": "pong",
+			"x":ctx.URLParam("firstname"),
+			"y":ctx.URLParam("lastname"),
+			"fullname":x +" "+ y,
+		})
+	})
+
+	app.Get("/hai", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{
+			"message": "halo",
+		})
+	})
+
+
+	// listen and serve on http://0.0.0.0:8080.
+	app.Run(iris.Addr(":8080"))
+}
+func main2(){
 	e := casbin.NewEnforcer("abac_model.conf", "basic_policy.csv")
 
 	sub := "wury" // the user that wants to access a resource.
